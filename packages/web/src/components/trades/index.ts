2ad894cb940e12ef156eed96128baa85058be27a
Marcus Franz <marcus.franz93@gmail.com>
2026-01-20 21:26:10 -0500

    feat: add trades component barrel export

---
+++ packages/web/src/components/trades/index.ts
@@ -0,0 +1,5 @@
+// packages/web/src/components/trades/index.ts
+export { TradeCard } from './TradeCard';
+export { TradeDetail } from './TradeDetail';
+export { CheckInBar } from './CheckInBar';
+export { GuidancePrompt } from './GuidancePrompt';
