88c4aeccf5d6e1503591eafeed09161257e46a9d
Marcus Franz <marcus.franz93@gmail.com>
2026-01-20 21:33:03 -0500

    feat: add opportunities component barrel export

---
+++ packages/web/src/components/opportunities/index.ts
@@ -0,0 +1,4 @@
+// packages/web/src/components/opportunities/index.ts
+export { OpportunityBrowser } from './OpportunityBrowser';
+export { OpportunityCard } from './OpportunityCard';
+export { FilterBar } from './FilterBar';
